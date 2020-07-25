7312c944dcd57554d68fc06ddd860e2e92621821
AsesorLGVR <68026156+AsesorLGVR@users.noreply.github.com>
2020-07-25 21:26:55 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,48 @@
-namespace myTiles {
-    //% blockIdentity=images._tile
-    export const tile0 = img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`
-}
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile0, function (sprite, location) {
-	
+scene.onOverlapTile(SpriteKind.Food, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, myTiles.transparency16)
+    mySprite.startEffect(effects.spray)
 })
-let mySprite = sprites.create(img`
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 d d d d d d d 1 1 1 1 
-. . 1 1 d d d d d d d d d 1 1 1 
-. . 1 d d d d d d d d d d 1 1 1 
-. . d d d f d d d f d d d 1 1 1 
-. . d d d d d d d d d d d 1 1 1 
-. . d d d d d d d d d d d 1 1 1 
-. . d d d f f f f d d d 1 1 1 . 
-. . d d d d d d d d d d d . . . 
-d d d d d d d d d d d d d d d . 
-d d d d d d d d d d d d d d d . 
-d . d d d d d d d d d d d . d . 
-d . d d d d d d d d d d d . d . 
-. . d d d d e d d d d d d . . . 
-. . d d d d d d d d d d d . . . 
-. . . d d d . d d d d . . . . . 
-`, SpriteKind.Player)
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+    . 1 1 1 1 d d d d d d d 1 1 1 1 
+    . . 1 1 d d d d d d d d d 1 1 1 
+    . . 1 d d d d d d d d d d 1 1 1 
+    . . d d d f d d d f d d d 1 1 1 
+    . . d d d d d d d d d d d 1 1 1 
+    . . d d d d d d d d d d d 1 1 1 
+    . . d d d f f f f d d d 1 1 1 . 
+    . . d d d d d d d d d d d . . . 
+    d d d d d d d d d d d d d d d . 
+    d d d d d d d d d d d d d d d . 
+    d . d d d d d d d d d d d . d . 
+    d . d d d d d d d d d d d . d . 
+    . . d d d d e d d d d d d . . . 
+    . . d d d d d d d d d d d . . . 
+    . . . d d d . d d d d . . . . . 
+    `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
-tiles.setTilemap(tiles.createTilemap(
-            hex`0800140002020808080802020208080808080802020c080808080b02020202080802020202080808080808020208080808080802020b080808080c02020202080802020202080808080808020208080808080802020c080808080b02020202080802020202080808080808020208080808080802020b080808080c0202020208080202020208080808080802020708080808070202020808080802020202020202020202`,
-            img`
-2 2 . . . . 2 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 2 2 . . 2 2 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 2 2 . . 2 2 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 2 2 . . 2 2 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 2 2 . . 2 2 2 
-2 . . . . . . 2 
-2 . . . . . . 2 
-2 2 . . . . 2 2 
-2 2 2 2 2 2 2 2 
-`,
-            [myTiles.tile0,sprites.builtin.brick,sprites.dungeon.floorLight5,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.doorOpenNorth,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.doorLockedNorth,sprites.dungeon.chestClosed,sprites.dungeon.doorLockedEast,sprites.dungeon.doorLockedSouth,sprites.dungeon.doorLockedWest],
-            TileScale.Sixteen
-        ))
+tiles.setTilemap(tiles.createTilemap(hex`0800140001010000000001010100000000000001010400000000030101010100000101010100000000000001010000000000000101030000000004010101010000010101010000000000000101000000000000010104000000000301010101000001010101000000000000010100000000000001010300000000040101010100000101010100000000000001010200000000020101010000000501010101010101010101`, img`
+    2 2 . . . . 2 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 2 2 . . 2 2 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 2 2 . . 2 2 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 2 2 . . 2 2 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 2 2 . . 2 2 2 
+    2 . . . . . . 2 
+    2 . . . . . . 2 
+    2 2 . . . . 2 2 
+    2 2 2 2 2 2 2 2 
+    `, [myTiles.tile0,sprites.dungeon.floorLight5,sprites.dungeon.doorOpenNorth,sprites.dungeon.doorLockedEast,sprites.dungeon.doorLockedSouth,sprites.dungeon.collectibleRedCrystal], TileScale.Sixteen))
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.doorOpenNorth)
 scene.cameraFollowSprite(mySprite)
