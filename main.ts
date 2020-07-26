7d69ebc16263322ac60af447b62252d3b5885997
AsesorLGVR <68026156+AsesorLGVR@users.noreply.github.com>
2020-07-26 14:17:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,7 +28,6 @@ mySprite = sprites.create(img`
     . . d d d d d d d d d d d . . . 
     . . . d d d . d d d d . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
 tiles.setTilemap(tiles.createTilemap(hex`0800140001010000000001010100000000000001010400000000030101010100000101010100000000000001010000000000000101030000000004010101010000010101010000000000000101000000000000010104000000000301010101000001010101000000000000010100000000000001010300000000040101010100000101010100000000000001010200000000020101010000000501010101010101010101`, img`
     2 2 . . . . 2 2 
     2 . . . . . . 2 
@@ -53,5 +52,8 @@ tiles.setTilemap(tiles.createTilemap(hex`080014000101000000000101010000000000000
     `, [myTiles.tile0,sprites.dungeon.floorLight5,sprites.dungeon.doorOpenNorth,sprites.dungeon.doorLockedEast,sprites.dungeon.doorLockedSouth,sprites.dungeon.collectibleRedCrystal], TileScale.Sixteen))
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.doorOpenNorth)
 scene.cameraFollowSprite(mySprite)
-mySprite.say("obvio")
+mySprite.say("Obvio")
 info.setScore(0)
+mySprite.ay = 800
+mySprite.ax = 0
+controller.moveSprite(mySprite)
